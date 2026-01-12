c549801f4094ce089d0b644a20437ad876100a6e
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-12 22:42:05 +0000

    init: right project

---
+++ src.dev/profiles/profile.mjs
@@ -0,0 +1,1 @@
+/** profiles can be modded together ... */
---
+++ src.dev/profiles/safe/self_check.mjs
@@ -0,0 +1,6 @@
+/** when in safe mode performs a real file system test to ensure it actually works 
+ * and something weird is not happening */
+
+export function self_check() {
+    // use vfs, expand to temp and another disk.. link test , and then remove ( hmm danger )    
+}
