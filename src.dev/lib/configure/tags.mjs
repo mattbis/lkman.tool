0e2d3d5a16ea1525401d6091ab5ff746db5f2547
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-22 14:41:27 +0000

    Create tags.mjs

---
+++ src.dev/lib/configure/tags.mjs
@@ -0,0 +1,4 @@
+// parse [thing] when using commands as 2nd argument ( after command ) 
+// can only be a-z, 0-6, _ - 
+// parsed as a safe string with stripping of any chars that arent pure ascii
+// TODO (matt): this should be release.
