7f8385bced9c7d2b32394ae3355fa04c2a6fc625
Matthew <863968+mattbis@users.noreply.github.com>
2026-01-28 02:50:08 +0000

    Update tags.mjs

--- src.dev/lib/configure/tags.mjs
+++ src.dev/lib/configure/tags.mjs
@@ -2,3 +2,6 @@
 // can only be a-z, 0-6, _ - 
 // parsed as a safe string with stripping of any chars that arent pure ascii
 // TODO (matt): this should be release.
+
+/** tags are how everything works...  you can do someting without a tag... by default they sit in one default tag - its much better to use
+custom stags.. to reduce the operation workload ... and what lib-change-descent is doing */
